8d3466f394db4724b0317c2121fd1a143b3ba3c2
Cathub-Script <glomi1807ok@gmail.com>
2026-02-11 12:28:55 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 
 
-local API_URL = "https://whitelist-api-1ght.onrender.com"
+local API_URL = "https://4fb4ad48-15f1-4fc1-b136-6fe244fbdc36-00-11xzf7zv4wcky.sisko.replit.dev"
 local maxRetries = 3
 local retryDelay = 2
 
@@ -121,3 +121,4 @@ else
     
     return false
 end
+
